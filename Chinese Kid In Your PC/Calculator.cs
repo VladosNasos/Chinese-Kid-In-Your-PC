bfd2abb852942d6e123a7644ee187a5611ca92e0
VladosNasos <bezzzubkovlad@gmail.com>
2023-07-03 19:36:38 +0300

    Add project files.

---
+++ Chinese Kid In Your PC/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Chinese_Kid_In_Your_PC
+{
+    public class Calculator
+    {
+        public double Calculate(double number1, double number2, string operation)
+        {
+            switch (operation)
+            {
+                case "Add":
+                    return number1 + number2;
+                case "Subtract":
+                    return number1 - number2;
+                case "Multiply":
+                    return number1 * number2;
+                case "Divide":
+                    if (number2 != 0)
+                        return number1 / number2;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero");
+                default:
+                    throw new InvalidOperationException("Invalid operation");
+            }
+        }
+    }
+}
---
+++ Chinese Kid In Your PC/ViewMode.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinese_Kid_In_Your_PC
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private double number1;
+        private double number2;
+        private string operation;
+        private double result;
+
+        public double Number1
+        {
+            get { return number1; }
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+                Calculate();
+            }
+        }
+
+        public double Number2
+        {
+            get { return number2; }
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+                Calculate();
+            }
+        }
+
+        public string Operation
+        {
+            get { return operation; }
+            set
+            {
+                operation = value;
+                OnPropertyChanged();
+                Calculate();
+            }
+        }
+
+        public double Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Calculate()
+        {
+            var calculator = new Calculator();
+            Result = calculator.Calculate(Number1, Number2, Operation);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
